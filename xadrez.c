701fb56ba54e69bd60f6a6ba830edabe5bb2a83e
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-03-24 12:18:30 +0000

    Atualizaçõs do nível aventureiro em xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int maxBispo = 5, maxTorre = 5, maxRainha = 8;
+    int movBispo = 0, movTorre = 0, movRainha = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Movimento do Bispo:\n");
+    while (movBispo < maxBispo){
+        printf("- Cima Direita\n");
+        movBispo++;
+    }
+    printf("\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Movimento da Torre:\n");
+    do {
+        printf("- Direita\n");
+        movTorre++;
+    } while (movTorre < maxTorre);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("\n");
+
+    printf("Movimento da Rainha:\n");
+    for (movRainha = 0; movRainha < maxRainha; movRainha++){
+        printf("- Esquerda\n");
+    }
+
+    int maxCavalo1 = 1, maxCavalo2 = 2;
+    int movCavalo1 = 0, movCavalo2 = 0;
+
+    printf("\nMovimento do Cavalo:\n");
+
+    for(movCavalo1 = 0; movCavalo1 < maxCavalo1; movCavalo1++){
+        while(movCavalo2 < maxCavalo2){
+            printf("- Cima\n");
+            movCavalo2++;
+        }
+        printf("- Direita\n");
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
